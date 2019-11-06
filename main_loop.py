f5ae5ef6c073000a8d1130f2b506c7df6d29ca50
Joseph Giubardo <jgiubard@terpmail.umd.edu>
2019-11-05 21:06:43 -0500

    Looper will now try again once after failing

--- main_loop.py
+++ main_loop.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from loop import MusicFile
+from loop import UnsuccessfulLoop
 
 
 def loop_track(filename):
@@ -9,17 +10,30 @@ def loop_track(filename):
         print("Loading {}...".format(filename))
         track = MusicFile(filename)
         track.calculate_max_frequencies()
-        start_offset, best_offset, best_corr = track.find_loop_point()
-        print("Found loop starting at {} sample {} and ending {} sample {} ({:.0f}% match)".format(
-            format_time(track.time_of_frame(start_offset)),
-            track.sample_of_frame(start_offset),
-            format_time(track.time_of_frame(best_offset)),
-            track.sample_of_frame(best_offset),
-            best_corr * 100))
-        write_points_to_file(track.sample_of_frame(start_offset), track.sample_of_frame(best_offset), filename)
-
+    # handles errors in loading the file
     except (TypeError, FileNotFoundError) as e:
         print("Error: {}".format(e))
+    # try to loop the file
+    try:
+        start_offset, best_offset, best_corr = track.find_loop_point()
+        print("Found loop starting at {} and ending {} ({:.0f}% match)".format(
+            format_time(track.time_of_frame(start_offset)),
+            format_time(track.time_of_frame(best_offset)),
+            best_corr * 100))
+        write_points_to_file(track.sample_of_frame(start_offset), track.sample_of_frame(best_offset), filename)
+    # if a loop isn't found, retry with different inputs
+    except UnsuccessfulLoop:
+        try:
+            print("Trying again")
+            start_offset, best_offset, best_corr = track.find_loop_point(100, 300)
+            print("Found loop starting at {} and ending {} ({:.0f}% match)".format(
+                format_time(track.time_of_frame(start_offset)),
+                format_time(track.time_of_frame(best_offset)),
+                best_corr * 100))
+            write_points_to_file(track.sample_of_frame(start_offset), track.sample_of_frame(best_offset), filename)
+        # if a loop still isn't found, give up
+        except UnsuccessfulLoop:
+            print("Failed to find loop")
 
 
 def loop_all_tracks():
@@ -36,6 +50,7 @@ def loop_all_tracks():
     print("Wrote to file")
 
 
+# returns time from seconds in the form of MM:SS.mlS
 def format_time(time_sec):
     return "{:02.0f}:{:06.3f}".format(
         time_sec // 60,
